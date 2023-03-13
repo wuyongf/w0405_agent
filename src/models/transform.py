6e9b87c3333b2087ee6abee736521027d6a41dbc
Yongfeng Wu <44566736+wuyongf@users.noreply.github.com>
2023-03-13 19:08:26 +0800

    1

---
+++ src/models/transform.py
@@ -0,0 +1,11 @@
+class Transform:
+    def __init__(self):
+        pass
+    
+    def origin_rv2rm(self, width, height, x, y, angle):
+        origin_rm_x = abs(x)/0.05
+        origin_rm_y = height - abs(y)/0.05
+        pass
+
+    def pos_rm2rv(self):
+        pass
