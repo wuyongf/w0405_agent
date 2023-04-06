fe6c36dd3472115b03f788b40d0fed4cde2853e4
Yongfeng Wu <44566736+wuyongf@users.noreply.github.com>
2023-04-06 14:41:53 +0800

    test1

--- useful_functions/mq_joystick.py
+++ useful_functions/mq_joystick.py
@@ -2,35 +2,35 @@ import paho.mqtt.client as mqtt
 import json
 import time
 
-# Set MQTT broker IP address and port number
-broker_address = "192.168.1.56"
-broker_port = 1883
 
-# Define topic and payload
-topic = "rvautotech/fobo/joystick"
-payload = {
-    "className": "com.rvautotech.fobo.amr.dto.JoystickDTO",
-    "object": 
-    {
-        "upDown": 0.5,
-        "leftRight": 0,
-        "turboOn": False
-    }
-}
+if __name__ == "__main__":
 
-# Convert payload to JSON string
-payload_str = json.dumps(payload)
+    # Set MQTT broker IP address and port number
+    broker_address = "192.168.1.56"
+    broker_port = 1883
 
-# Create MQTT client and connect to broker
-client = mqtt.Client()
-client.connect(broker_address, broker_port)
+    # Define topic and payload
+    topic = "rvautotech/fobo/joystick"
+    payload = {
+            "upDown": 0,
+            "leftRight": 0,
+            "turboOn": False
+        }
 
-# Publish message
-while(True):
-    client.publish(topic, payload_str)
-    time.sleep(0.01)
-    print('1')
+    # Convert payload to JSON string
+    payload_str = json.dumps(payload)
+
+    # Create MQTT client and connect to broker
+    client = mqtt.Client('joystick_publisher')
+    client.connect(broker_address, broker_port)
+    client.loop_start()
+
+    # Publish message
+    while(True):
+        client.publish(topic, payload_str)
+        time.sleep(0.01)
+        print('1')
 
 
-# Disconnect from broker
-client.disconnect()
+    # Disconnect from broker
+    client.disconnect()
--- useful_functions/rm_task_creation/mq_logger.py
+++ useful_functions/rm_task_creation/mq_logger.py
@@ -26,7 +26,8 @@ class MQLOGGER:
     def __subscribe_task(self):
         while True:
             # publisher.publish("/robot/status", robotStatus)
-            self.mq_subscriber.subscribe([("/robot/status", 2), ("/rm/task", 2), ("/robot/task/status", 2)])
+            # self.mq_subscriber.subscribe([("/robot/status", 2), ("/rm/task", 2), ("/robot/task/status", 2)])
+            self.mq_subscriber.subscribe([("/rm/control", 2), ("/rm/task", 0), ("/robot/task/status", 0)])
             # self.mq_subscriber.subscribe(self.mq_subs_topic, 2)
             time.sleep(1)
     
