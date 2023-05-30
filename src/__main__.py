aef206a1114c1189fe18345b11e330e4fd01073a
NW0405 <133304981+NW0405@users.noreply.github.com>
2023-05-30 17:34:37 +0800

    modified mian func

--- src/__main__.py
+++ src/__main__.py
@@ -3,31 +3,57 @@ import logging.handlers as handlers
 import os
 import time
 import sys
+import threading
+import subprocess
 # yf
 import src.utils.methods as umethods
 import handlers.status_handler as status_handler
 import handlers.remote_control_handler as remote_control_handler
 import handlers.task_handler3 as task_handler
 
+def run_robot_agent():
+
+
+    # Change to the desired directory
+    directory = "~/dev/robot-agent-v2.0/"
+    os.chdir(os.path.expanduser(directory))
+
+    # Run the shell script
+    script_path = "./run-agent.sh"
+    subprocess.run(["bash", script_path])
+
+
+
 if __name__ == '__main__':
+
+    # # Store the current directory
+    # previous_directory = os.getcwd()
+
+    # # Create a thread and start it
+    # thread = threading.Thread(target=run_robot_agent)
+    # thread.start()
+
+    # # Change back to the previous directory
+    # os.chdir(previous_directory)
+
     # Sleep for 30 seconds for Pi to connect to MiR network...
     time.sleep(2)
 
-    # Setup logging configs
-    if not os.path.exists('../../logs'):
-        os.makedirs('../../logs')
-    logname = "../../logs/nw-rm-rv-app.log"
-    formatter = logging.Formatter('[%(asctime)s] [%(levelname)s] %(filename)s:%(lineno)d %(funcName)s(): %(message)s',
-                                  datefmt='%d-%b-%y %H:%M:%S')
-    logHandler = handlers.TimedRotatingFileHandler(logname, when='D', interval=1, backupCount=30, encoding='utf-8')
-    logHandler.setFormatter(formatter)
-    logStreamHandler = logging.StreamHandler(sys.stdout)
-    logStreamHandler.setFormatter(formatter)
+    # # Setup logging configs
+    # if not os.path.exists('../../logs'):
+    #     os.makedirs('../../logs')
+    # logname = "../../logs/nw-rm-rv-app.log"
+    # formatter = logging.Formatter('[%(asctime)s] [%(levelname)s] %(filename)s:%(lineno)d %(funcName)s(): %(message)s',
+    #                               datefmt='%d-%b-%y %H:%M:%S')
+    # logHandler = handlers.TimedRotatingFileHandler(logname, when='D', interval=1, backupCount=30, encoding='utf-8')
+    # logHandler.setFormatter(formatter)
+    # logStreamHandler = logging.StreamHandler(sys.stdout)
+    # logStreamHandler.setFormatter(formatter)
 
-    logger = logging.getLogger('')
-    logger.addHandler(logStreamHandler)
-    logger.addHandler(logHandler)
-    logger.setLevel(logging.INFO)
+    # logger = logging.getLogger('')
+    # logger.addHandler(logStreamHandler)
+    # logger.addHandler(logHandler)
+    # logger.setLevel(logging.INFO)
 
     # # Loading config files
     config = umethods.load_config('../conf/config.properties')
@@ -37,7 +63,6 @@ if __name__ == '__main__':
     status_handler = status_handler.StatusHandler(config, port_config)
     status_handler.start()
 
-    
     # # Task handler subscribes and execute task, also report task status to robotmanager
     task_handler = task_handler.TaskHandler(config, port_config)
     task_handler.start()
@@ -48,6 +73,6 @@ if __name__ == '__main__':
     remote_control_handler.start()
 
     # Successfully started the app
-    logging.getLogger('').info("rm-mir-app successfully started!")
+    # logging.getLogger('').info("rm-mir-app successfully started!")
     print('main finished')
 
