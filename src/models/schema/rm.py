1af263428e24bf01987d3e31e33fac95db42b7de
Kong <tckong0325@gmail.com>
2023-07-21 16:37:06 +0800

    rm_followme

--- src/models/schema/rm.py
+++ src/models/schema/rm.py
@@ -9,22 +9,27 @@
 import json
 import uuid
 
+
 class Status:
     def __init__(self, batteryPct, state, mapPose):
         self.batteryPct = batteryPct
         self.state = state
         self.mapPose = mapPose
+
     def to_json(self):
         return json.dumps(self.__dict__, default=lambda o: o.__dict__)
 
+
 class mapPose:
-    def __init__(self, mapId = '', x = 0.0, y = 0.0, heading = 0.0):
+    def __init__(self, mapId='', x=0.0, y=0.0, heading=0.0):
         self.mapId = mapId
         self.x = x
         self.y = y
         self.heading = heading
+
     def to_json(self):
         return json.dumps(self.__dict__)
+
 
 # class Task1:
 #     def __init__(self, taskId, task_type, parameters):
@@ -40,12 +45,14 @@ class mapPose:
 #         self.taskType = task_type
 #         self.parameters = parameters
 
+
 class TaskStatus:
-    def __init__(self, taskId, taskType, taskStatusType, errMsg = ''):
+    def __init__(self, taskId, taskType, taskStatusType, errMsg=''):
         self.taskId = taskId
         self.taskType = taskType
         self.taskStatusType = taskStatusType
         self.errMsg = errMsg
+
 
 class JobUpdate:
     def __init__(self, id, name, tasks):
@@ -53,9 +60,10 @@ class JobUpdate:
         self.name = name
         self.tasks = tasks
 
+
 # To create Job/Task
 class TaskParams:
-    def __init__(self, dct = None):
+    def __init__(self, dct=None):
         if dct is None: return None
         else:
             self.mapId = dct['mapId']
@@ -64,10 +72,11 @@ class TaskParams:
             self.y = dct['y']
             self.heading = dct['heading']
 
+
 # to receive mtqq task json
 class Task:
-    def __init__(self, dct = None):
-        if dct is None: 
+    def __init__(self, dct=None):
+        if dct is None:
             print('task_json is NULL')
             return None
         else:
@@ -75,6 +84,7 @@ class Task:
             self.scheduleType = dct['scheduleType']
             self.priority = dct['priority']
             self.taskType = dct['taskType']
+
 
 # Built-In-Parameters: RM-GOTO/RM-LOCALIZE
 class RMGOTO:
@@ -85,33 +95,45 @@ class RMGOTO:
         self.y = y
         self.heading = heading
 
+
 class RMREMOTEESTOP:
     def __init__(self, estop=False):
         self.estop = estop
+
 
 class RMLIGHT:
     def __init__(self, light=False):
         self.light = light
 
 
+class RMFOLLOWME:
+    def __init__(self, mode=False, pairingState=False):
+        self.mode = mode
+        self.state = pairingState
+
+
 # Event
 class Meida:
-    def __init__(self, filePath, type, title, view360 = False):
+    def __init__(self, filePath, type, title, view360=False):
         self.filePath = filePath
-        self.type = type # 1: image 2: video
+        self.type = type  # 1: image 2: video
         self.title = title
         self.__dict__['360View'] = view360
+
+
 class Event:
-    def __init__(self, title, severity, description, mapPose = mapPose, medias = [], metadata = {}):
+    def __init__(self, title, severity, description, mapPose=mapPose, medias=[], metadata={}):
         self.eventId = str(uuid.uuid1())
         self.title = title
-        self.severity = severity # 1: critical 2: normal
+        self.severity = severity  # 1: critical 2: normal
         self.description = description
         self.medias = medias
         self.mapPose = mapPose
         self.metadata = metadata
+
     def to_json(self):
         return json.dumps(self.__dict__, default=lambda o: o.__dict__)
+
 
 if __name__ == '__main__':
     # media_info_list = []
@@ -123,7 +145,7 @@ if __name__ == '__main__':
     # to publish an event
     # 1. event title.   (str)
     # 2. severity.      (int) (1: critical 2: normal)
-    # 3. description    (str) 
+    # 3. description    (str)
     # 4. mapPose_json   (str)
     # 5. medias_json    (str) (optional)
     # 6. metadata       (str) (optional)
@@ -140,9 +162,10 @@ if __name__ == '__main__':
 
     # 5.
     medias = []
-    medias.append(Meida("D:/Dev/w0405_agent/src/data/event_images/20230331_145659_0001/front_right.png", 1, "Front Right"))
-    
+    medias.append(
+        Meida("D:/Dev/w0405_agent/src/data/event_images/20230331_145659_0001/front_right.png", 1, "Front Right"))
+
     print('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
 
-    event1_json = Event(title, severity , description, map_pose, medias).to_json()
+    event1_json = Event(title, severity, description, map_pose, medias).to_json()
     print(event1_json)
