21635efd93ee7f0de7c7eee00f657a5d9eaaca86
yf <yf@g15ubuntu>
2023-11-01 18:29:52 +0800

    [camera] upated

--- src/ai_module/lift_noise/CameraRecorder.py
+++ src/ai_module/lift_noise/CameraRecorder.py
@@ -1,5 +1,53 @@
 import cv2
 
+class RGBCamera:
+
+    def __init__(self, index):
+        self.index = index
+        self.cap = cv2.VideoCapture(self.index)
+        # check
+        if not self.cap.isOpened():
+            print(f"Error: Could not open camera {index}.")
+            pass
+        # Define the codec and create VideoWriter objects for recording
+        self.fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        self.out = cv2.VideoWriter('output1.avi', self.fourcc, 20.0, (640, 480))
+
+        # logic
+        self.flag = True
+
+    def update_save_path(self, path):
+        self.path = path # "/home/nw/Desktop/Videos"
+        pass
+
+    def start_recording(self):   
+
+        while True:
+            # Read frames from the first camera
+            ret, frame = self.cap.read()
+
+            # If both frames were read successfully, record them
+            if ret:
+                self.out.write(frame)
+
+                # # Display the frames (optional)
+                # cv2.imshow(f'Camera {self.index}', frame)
+
+    def save_record(self):
+        self.stream.stop_stream()
+        self.stream.close()
+        self.audio.terminate()
+
+        file_name = os.path.join(self.path, "recording_" + str(time.time()) + ".wav")
+        sound_file = wave.open(file_name, "wb")
+        sound_file.setnchannels(self.n_channels)
+        sound_file.setsampwidth(self.audio.get_sample_size(pyaudio.paInt16))
+        sound_file.setframerate(self.sampling_rate)
+        sound_file.writeframes(b''.join(self.frames))
+        sound_file.close()
+
+        print("Saved" + file_name)
+
 # Open the first camera
 cap1 = cv2.VideoCapture(0)  # Use the appropriate index for your first camera
 
