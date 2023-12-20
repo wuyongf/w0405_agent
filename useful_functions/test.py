2c38ceedde14549b91ddd377b8c619ec16090a10
yf <yf@g15ubuntu>
2023-12-20 12:08:58 +0800

    updated

--- useful_functions/test.py
+++ useful_functions/test.py
@@ -1,7 +1,47 @@
+# importing required modules 
+from PyPDF2 import PdfReader 
+import csv
 
+def split2list(string: str):
+    
+    lines = string.split('\n')  # Splitting the string into lines
 
-number = '2'
+    # Splitting the lines further to extract the desired elements
+    list_a = [
+        lines[2],  # Index 2 contains '83576774'
+        lines[1].split(' / ')[1],  # Splitting index 1 using ' / ' and taking the second part 'H-90001'
+        lines[0],  # Index 0 contains 'Finish Line Check High Waisted Bottoms'
+        lines[1].split(' / ')[0]  # Splitting index 1 using ' / ' and taking the first part 'XXS'
+    ]
 
-n2 = int(number)
+    return list_a
 
-print(n2)
+if __name__ == '__main__':
+    # creating a pdf reader object 
+    reader = PdfReader('Tops_Suits.pdf') 
+    
+    # printing number of pages in pdf file 
+    print(len(reader.pages)) 
+    
+    final_list = []
+    for index in range(len(reader.pages)):
+        # getting a specific page from the pdf file 
+        page = reader.pages[0]       
+        # extracting text from page 
+        text = page.extract_text()
+
+        temp_list = split2list(text)
+
+        final_list.append(temp_list)
+    
+    print(final_list)
+
+    # Define the file name to save the CSV
+    file_name = 'temp.csv'
+
+    # Writing the data to a CSV file
+    with open(file_name, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(final_list)
+
+    print(f"CSV file '{file_name}' has been created successfully.")
